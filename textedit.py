6bf59d68da303a52d80fc175823397113794017e
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-11-19 14:34:12 +0530

    Add input field

--- textedit.py
+++ textedit.py
@@ -2,22 +2,22 @@ from flask import *
 import os
 app = Flask(__name__)
 
-
 @app.route('/')
 def editPython():
-        return render_template('template.html',prog='',remarks='',output='')
-
+        return render_template('template.html',prog='',input='',remarks='',output='')
 
 @app.route('/result', methods=['POST','GET'])
 def compute():
     if request.method == 'POST':
-        result = request.form['prog'] 
-        open("code.py",'w').write(result)
-        os.system('python3 code.py 1> output.file 2> remarks.file')
+        result = request.form
+        result_dict = dict(result.items())
+        open("code.py",'w').write(result_dict['prog'])
+        open("input.file",'w').write(result_dict['input'])
+        os.system('python code.py < input.file 1> output.file 2> remarks.file')
         remark = open('remarks.file','r').read()
         outputs = open('output.file','r').read()
         os.system('rm code.py')
-        return render_template('template.html',prog=result,remarks=remark,output=outputs)
-    
+        return render_template('template.html',prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
+
 if __name__ == '__main__':
     app.run(debug = False)
