34bb92432aef2578bf2b837e2ba5425d93a423aa
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-11-19 14:39:50 +0530

    Add cleanup code

--- textedit.py
+++ textedit.py
@@ -16,7 +16,7 @@ def compute():
         os.system('python code.py < input.file 1> output.file 2> remarks.file')
         remark = open('remarks.file','r').read()
         outputs = open('output.file','r').read()
-        os.system('rm code.py')
+        os.system('rm code.py *.file')
         return render_template('template.html',prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
 
 if __name__ == '__main__':
