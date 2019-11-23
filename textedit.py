1d8f434de35de856c3238a8882dbff4b62a422b5
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-11-23 13:17:59 +0530

    Implement Support for C and C++

--- textedit.py
+++ textedit.py
@@ -4,28 +4,40 @@ app = Flask(__name__)
 
 languages = ['Python', 'C', 'C++']
 
-#@app.route('/')
-#def editPython():
-#        return render_template('template.html',languages=languages,current_lang='',prog='',input='',remarks='',output='')
-
 @app.route('/', methods=['POST','GET'])
 def compute():
     if request.method == 'POST':
         result = request.form
         result_dict = dict(result.items())
         lang = result_dict['languages']
+        # Validate if input field is empty or not
+        # Check when compilation fails
         if lang == "Python":
             open("code.py",'w').write(result_dict['prog'])
             open("input.file",'w').write(result_dict['input'])
-            os.system('python code.py < input.file 1> output.file 2> remarks.file')
-            remark = open('remarks.file','r').read()
-            outputs = open('output.file','r').read()
-            os.system('rm code.py *.file')
-            return render_template('template.html',languages=languages,current_lang=lang,prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
+            os.system("python code.py < input.file 1> output.file 2> remarks.file")
+            remark = open("remarks.file",'r').read()
+            outputs = open("output.file",'r').read()
+            os.system("rm code.py *.file")
+            return render_template("template.html",languages=languages,current_lang=lang,prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
         elif lang == "C":
-            print("C is in dev")
+            open("code.c",'w').write(result_dict['prog'])
+            os.system("gcc code.c -o code 1> output.file 2> remarks.file")
+            open("input.file",'w').write(result_dict['input'])
+            os.system("code.exe < input.file 1>> output.file 2>> remarks.file")
+            remark = open("remarks.file",'r').read()
+            outputs = open("output.file",'r').read()
+            os.system("rm code* *.file")
+            return render_template("template.html",languages=languages,current_lang=lang,prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
         elif lang == "C++":
-            print("C++ is in dev")
+            open("code.cpp",'w').write(result_dict['prog'])
+            os.system("g++ code.cpp -o code 1> output.file 2> remarks.file")
+            open("input.file",'w').write(result_dict['input'])
+            os.system("code.exe < input.file 1>> output.file 2>> remarks.file")
+            remark = open("remarks.file",'r').read()
+            outputs = open("output.file",'r').read()
+            os.system("rm code* *.file")
+            return render_template("template.html",languages=languages,current_lang=lang,prog=result_dict['prog'],input=result_dict['input'],remarks=remark,output=outputs)
         else:
             print("You are one kind of a tester")
         return render_template('template.html',languages=languages,current_lang='',prog='',input='',remarks='',output='')
