973fd9f5ebce181554389bf93af905404f8289a5
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-11-26 11:32:24 +0530

    Prepare App for AWS deploy
    * Add config under .ebextensions to install packages : gcc-c++ using yum package manager
    * Ignore virt directory (virtual env)
    * Remove temporary files
    * Add static directory for css content
    * Fetch static content in template.html

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import *
 import os
-app = Flask(__name__)
+application = Flask(__name__)
 
 languages = ['Python', 'C', 'C++']
 
-@app.route('/', methods=['POST','GET'])
+@application.route('/', methods=['POST','GET'])
 def compute():
     if request.method == 'POST':
         result = request.form
@@ -24,7 +24,7 @@ def compute():
             open("code.c",'w').write(result_dict['prog'])
             os.system("gcc code.c -o code 1> output.file 2> remarks.file")
             open("input.file",'w').write(result_dict['input'])
-            os.system("code.exe < input.file 1>> output.file 2>> remarks.file")
+            os.system("./code < input.file 1>> output.file 2>> remarks.file")
             remark = open("remarks.file",'r').read()
             outputs = open("output.file",'r').read()
             os.system("rm code* *.file")
@@ -33,7 +33,7 @@ def compute():
             open("code.cpp",'w').write(result_dict['prog'])
             os.system("g++ code.cpp -o code 1> output.file 2> remarks.file")
             open("input.file",'w').write(result_dict['input'])
-            os.system("code.exe < input.file 1>> output.file 2>> remarks.file")
+            os.system("./code < input.file 1>> output.file 2>> remarks.file")
             remark = open("remarks.file",'r').read()
             outputs = open("output.file",'r').read()
             os.system("rm code* *.file")
@@ -44,4 +44,4 @@ def compute():
     elif request.method == 'GET':
         return render_template('template.html',languages=languages,current_lang='',prog='',input='',remarks='',output='')
 if __name__ == '__main__':
-    app.run(debug = False)
+    application.run(debug = False)
